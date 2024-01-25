722465ac12a0451f62fedd89a3e2eea8444dd7dd
The Passionate Programmer <wamungai10@gmail.com>
2024-01-25 10:34:41 +0300

    Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import { Router, Route,Switch } from 'react-router-dom';
+import{ Route,Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './components/Home';
+import Destination from './components/Destination';
+import Technology from './components/Technology';
+import Crew from './components/Crew';
+import Error from './components/Error';
 
 function App() {
   return (
     <>
-    <Header />
+  
+      <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/destination"  element={<Destination />}/>
+        <Route path="/crew"  element={<Crew />}/>
+        <Route path="/technology"  element={<Technology />}/>
+        <Route path="/*"  element={<Error />}/>
+      </Routes>
+      </main>
+    
+    
     </>
   );
 }
